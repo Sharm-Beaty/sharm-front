aadbefbea7f050f1fc34f8f8344922b3f404c0bd
DM <dynamo@DMs-MacBook-Air.local>
2023-11-04 15:31:28 +0200

    update swiper version to 11 and change banner styles logic

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -2,45 +2,51 @@
 "use client";
 import "swiper/css";
 import "swiper/css/pagination";
-import { Pagination, Autoplay } from "swiper/modules";
+import { Pagination, Autoplay, EffectCoverflow } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "./Banner.scss";
 import { data } from "./mockData";
 import Link from "next/link";
 
 export const Banner = () => {
-  return (
-    <section className='banner'>
-      <Swiper
-        pagination
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-          pauseOnMouseEnter: true,
-        }}
-        loop
-        modules={[Pagination, Autoplay]}
-        spaceBetween={20}
-      >
-        {data.map((info) => (
-          <SwiperSlide key={info.src}>
-            <img
-              className='banner__bg'
-              src={info.src}
-              alt='Image with beauty woman'
-            />
-            <div className='banner__content'>
-              <h4 className='banner__heading'>{info.header}</h4>
-              <Link
-                href={`/${info.url}`}
-                className='banner__button'
-              >
-                {info.btn}
-              </Link>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </section>
-  );
+    return (
+        <section className="banner">
+            <Swiper
+                pagination={{ clickable: true }}
+                effect="coverflow"
+                breakpoints={{
+                    0: {
+                        autoplay: false,
+                    },
+                    768: {
+                        autoplay: {
+                            disableOnInteraction: false,
+                            pauseOnMouseEnter: true,
+                        },
+                    },
+                }}
+                autoplay
+                loop
+                modules={[Pagination, Autoplay, EffectCoverflow]}
+                spaceBetween={10}
+            >
+                {data.map((info) => (
+                    <SwiperSlide key={info.desktopSrc}>
+                        <Link href={`/${info.url}`}>
+                            <img
+                                className="banner__bg desktop-only"
+                                src={info.desktopSrc}
+                                alt="Background photo"
+                            />
+                            <img
+                                className="banner__bg mobile-only"
+                                src={info.mobileSrc || info.desktopSrc}
+                                alt="Background photo"
+                            />
+                        </Link>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </section>
+    );
 };
--- src/components/Banner/mockData.ts
+++ src/components/Banner/mockData.ts
@@ -1,20 +1,17 @@
 export const data = [
   {
-    src: "https://images.unsplash.com/photo-1551184451-76b762941ad6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80",
+    desktopSrc: "https://images.unsplash.com/photo-1551184451-76b762941ad6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80",
+    mobileSrc: "https://images.unsplash.com/photo-1596462502278-27bfdc403348?auto=format&fit=crop&q=80&w=2500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     url: "",
-    header: "Найкращі пропозиції цього літа",
-    btn: "Пропозиції",
   },
   {
-    src: "https://images.unsplash.com/photo-1611605472181-7962c1c85ed5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    desktopSrc: "https://images.unsplash.com/photo-1611605472181-7962c1c85ed5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    mobileSrc: "",
     url: "",
-    header: "Топові пропозиції",
-    btn: "подробніше",
   },
   {
-    src: "https://plus.unsplash.com/premium_photo-1661768100045-2eaed3d2eb97?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    desktopSrc: "https://plus.unsplash.com/premium_photo-1661768100045-2eaed3d2eb97?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    mobileSrc: "",
     url: "",
-    header: "Відкрий для себе",
-    btn: "Акції",
   },
 ];
