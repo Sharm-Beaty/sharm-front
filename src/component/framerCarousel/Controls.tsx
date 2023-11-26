1af1f05a0b25ef6fd14640013eb38d77e0250b27
Romeo <104751985+semklim@users.noreply.github.com>
2023-11-26 20:01:12 +0200

    add wipes to carousel

--- src/component/framerCarousel/Controls.tsx
+++ src/component/framerCarousel/Controls.tsx
@@ -1,49 +1,12 @@
-import React from 'react';
-import { Product } from '../Main page/ProductCarousel/types/types';
 import { ArrowLeft, ArrowRight } from '../svg';
 import cls from './Controls.module.scss';
-type ControlsProps = {
-	sliderData: Product[];
-	data: Product[];
-	handleData: React.Dispatch<React.SetStateAction<Product[]>>;
-	currentIndex: number;
-	setCurrentIndex: React.Dispatch<React.SetStateAction<number>>;
-	slidesPerView: number;
-};
 
-function Controls({
-	sliderData,
-	handleData,
-	currentIndex,
-	setCurrentIndex,
-	slidesPerView,
-}: ControlsProps) {
-	const handlePrev = () => {
-		const index = (currentIndex - 1 + sliderData.length) % sliderData.length;
-		setCurrentIndex((prev) => (prev = index));
-		updateView(index);
-	};
+interface ControlsProps {
+	handlePrev: () => void;
+	handleNext: () => void;
+}
 
-	const handleNext = () => {
-		const index = (currentIndex + 1) % sliderData.length;
-		setCurrentIndex((prev) => (prev + 1) % sliderData.length);
-		updateView(index);
-	};
-
-	function updateView(index: number) {
-		let endIndex = index + slidesPerView;
-
-		if (endIndex > sliderData.length) {
-			// If yes, wrap around to the beginning
-			endIndex = endIndex - sliderData.length;
-			// Display sliderData from currentIndex to the end and then from the beginning to fill up to slidesPerView
-			handleData(() => [...sliderData.slice(index), ...sliderData.slice(0, endIndex)]);
-		} else {
-			// Display sliderData normally
-			handleData(() => sliderData.slice(index, endIndex));
-		}
-	}
-
+const Controls: React.FC<ControlsProps> = ({ handlePrev, handleNext }) => {
 	return (
 		<div className={cls.navWrap}>
 			<div className={cls.buttonPrev}>
@@ -54,6 +17,6 @@ function Controls({
 			</div>
 		</div>
 	);
-}
+};
 
 export default Controls;
--- src/component/framerCarousel/MCardsCarousel.tsx
+++ src/component/framerCarousel/MCardsCarousel.tsx
@@ -2,17 +2,22 @@
 import cls from './MCardsCarousel.module.scss';
 import { classNames } from '@/helpers';
 import { Product } from '../Main page/ProductCarousel/types/types';
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { MSlide } from './Slide/Slide';
 import Controls from './Controls';
-import { AnimatePresence, HTMLMotionProps, motion } from 'framer-motion';
+import { AnimatePresence, HTMLMotionProps, PanInfo, motion } from 'framer-motion';
 
 interface MCardsCarouselProps extends HTMLMotionProps<'section'> {
 	className?: string;
 	productsList: Product[];
-	slidesPerView: number;
+	slidesPerView?: number;
 	spaceBetween?: number;
 }
+
+const swipeConfidenceThreshold = 10000;
+const swipePower = (offset: number, velocity: number) => {
+	return Math.abs(offset) * velocity;
+};
 
 const MCardsCarousel: React.FC<MCardsCarouselProps> = (props) => {
 	const { className, productsList, spaceBetween = 75, ...otherProps } = props;
@@ -23,29 +28,54 @@ const MCardsCarousel: React.FC<MCardsCarouselProps> = (props) => {
 	}
 
 	const [currentIndex, setCurrentIndex] = useState(0);
-	const productsInView = productsList.slice(0, slidesPerView + 1);
-	const [data, setData] = React.useState<Product[]>(
-		productsList.slice(0, productsInView.length - 1),
-	);
+	const productsInView = productsList.slice(0, slidesPerView);
+	const [data, setData] = useState<Product[]>(productsInView);
+
+	const handlePrev = () =>
+		setCurrentIndex((prev) => (prev - 1 + productsList.length) % productsList.length);
+	const handleNext = () => setCurrentIndex((prev) => (prev + 1) % productsList.length);
+
+	const onSwipe = (e: MouseEvent | TouchEvent | PointerEvent, { offset, velocity }: PanInfo) => {
+		const swipe = swipePower(offset.x, velocity.x * 10);
+
+		if (swipe > -swipeConfidenceThreshold) {
+			handlePrev();
+		} else if (swipe < swipeConfidenceThreshold) {
+			handleNext();
+		}
+	};
+
+	useEffect(() => {
+		let endIndex = currentIndex + slidesPerView;
+
+		if (endIndex > productsList.length) {
+			// If yes, wrap around to the beginning
+			endIndex = endIndex - productsList.length;
+			// Display productsList from currentIndex to the end and then from the beginning to fill up to slidesPerView
+			setData(() => [...productsList.slice(currentIndex), ...productsList.slice(0, endIndex)]);
+		} else {
+			// Display productsList normally
+			setData(() => productsList.slice(currentIndex, endIndex));
+		}
+	}, [currentIndex, productsList, slidesPerView]);
 
 	return (
 		<motion.section className={classNames(cls.mCardsCarousel, [className])} {...otherProps}>
 			<h2 className={cls.mainTitle}>Framer Motion Carousel</h2>
-			<motion.div className={cls.carousel}>
+			<motion.div
+				className={cls.carousel}
+				drag='x'
+				dragConstraints={{ left: 0, right: 0 }}
+				dragElastic={1}
+				onDragEnd={onSwipe}
+			>
 				<AnimatePresence mode='popLayout'>
 					{data.map((data) => {
 						return <MSlide key={data.img} product={data} />;
 					})}
 				</AnimatePresence>
 			</motion.div>
-			<Controls
-				data={data}
-				handleData={setData}
-				sliderData={productsList}
-				currentIndex={currentIndex}
-				setCurrentIndex={setCurrentIndex}
-				slidesPerView={slidesPerView}
-			/>
+			<Controls handlePrev={handlePrev} handleNext={handleNext} />
 		</motion.section>
 	);
 };
