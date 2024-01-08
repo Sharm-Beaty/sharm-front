0811e0dcb466ffdca2ea6b00a89159464fe4fa8c
Valerii Skliarov <valeriisklyarov@gmail.com>
2024-01-08 14:14:36 +0200

    Refactored localization code in i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,12 +1,10 @@
 import {getRequestConfig} from 'next-intl/server';
-import {notFound} from "next/navigation";
 import {locales} from '../i18n/config';
-import NotFound from "@/app/not-found";
 
 
 const default_locale = 'ua';
 export default getRequestConfig(async ({locale}) => {
-    // if (!locales.includes(locale as any)) NotFound();
+    // if (!locales.includes(locale as any)) NotFound(); //TODO delete after approve :vs
     if (!locales.includes(locale as any)) locale = default_locale;
     const messages = {
         ...(await import(`../messages/${locale}.json`)).default,
